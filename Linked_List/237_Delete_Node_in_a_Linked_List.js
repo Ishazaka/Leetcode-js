1eb5b94f791e8b56f6b38aa9a126ca87bd728616
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2024-09-12 12:31:18 -0500

    Medium

--- Linked_List/237_Delete_Node_in_a_Linked_List.js
+++ Linked_List/237_Delete_Node_in_a_Linked_List.js
@@ -1,4 +1,3 @@
-
 /**
  * Definition for singly-linked list.
  * function ListNode(val) {
@@ -19,4 +18,6 @@ var deleteNode = function(node) {
 };
 
 
-//Explanation:  We are storing the next node value into this node value and then just linking this node to the node.next.next in this way, we will remvoe the node.next becuase we have store this value into the node which we have access
+//Explanation:  We are storing the next node value into this node value and then just linking this node to the node.next.next 
+// in this way, we will remvoe the node.next becuase we have store this value into the node which we have access
+// no need to return anything
