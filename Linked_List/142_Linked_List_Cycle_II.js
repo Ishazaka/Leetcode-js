e0e55e45d58c096abf2858aecaa6769b5dd0fbcf
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2024-09-15 22:07:37 -0400

    Create 142_Linked_List_Cycle_II.js

---
+++ Linked_List/142_Linked_List_Cycle_II.js
@@ -0,0 +1,64 @@
+
+//142. Linked List Cycle II
+
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle = function(head) {
+      
+    let fast = head;
+    let slow = head;
+
+   let pointer = head;
+
+    if(!head) return null;
+    if(!head.next) return null;
+
+   while(fast && fast.next){
+      slow = slow.next;
+      fast = fast.next.next;
+      if(fast === slow) break;
+
+   }
+   if(fast != slow) return null
+
+     
+   while(pointer !== slow){
+      pointer = pointer.next;
+      slow = slow.next
+   }  
+
+return slow;
+
+};
+
+
+
+// 2nd solution which took 51 ms
+
+
+var detectCycle = function(head) {
+    if(head==null||head.next==null)
+        return null;
+    let slow=head;
+    let fast=head;
+    let entry = head;
+    
+    while(fast!=null&&fast.next!=null)
+    {
+        fast = fast.next.next;
+        slow =slow.next;
+        if(fast==slow)
+        {
+            while(slow!=entry)
+            {
+                slow = slow.next;
+                entry=entry.next;
+            }
+            return entry;
+        }
+    }
+    return null;
+};
