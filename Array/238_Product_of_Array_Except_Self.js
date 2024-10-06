8bc027e5afa6e4ca09db67d9809c82c64da8743a
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2024-10-06 18:55:44 -0400

    Medium

--- Array/238_Product_of_Array_Except_Self.js
+++ Array/238_Product_of_Array_Except_Self.js
@@ -1,5 +1,7 @@
 
-// Brute Force  // O(n square)
+// Brute Force  
+// Time complexity: O(n square)
+// Space complexity:O(1)
 
 class Solution {
     /**
@@ -24,5 +26,39 @@ class Solution {
 }
 
 
+// Division  
+// Time complexity: O(n)
+// Space complexity:O(1)
 
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+        let prod = 1;
+        let zeroCount = 0;
+        for (let num of nums) {
+            if (num !== 0) {
+                prod *= num;
+            } else {
+                zeroCount++;
+            }
+        }
+
+        if (zeroCount > 1) {
+            return Array(nums.length).fill(0); 
+        }
+
+        const res = new Array(nums.length);
+        for (let i = 0; i < nums.length; i++) {
+            if (zeroCount > 0) {
+                res[i] = (nums[i] === 0) ? prod : 0;
+            } else {
+                res[i] = prod / nums[i];
+            }
+        }
+        return res;
+    }
+}
 
