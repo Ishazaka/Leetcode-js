97eb2175b89914f3679ae538cd7eef4ad175913c
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2024-10-09 08:16:33 -0400

    Medium

--- Array/238_Product_of_Array_Except_Self.js
+++ Array/238_Product_of_Array_Except_Self.js
@@ -62,3 +62,62 @@ class Solution {
     }
 }
 
+
+// Prefix and Suffix
+Time complexity: O(n)
+Space complexity: O(n)
+
+
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+        const n = nums.length;
+        const res = new Array(n);
+        const pref = new Array(n);
+        const suff = new Array(n);
+
+        pref[0] = 1;
+        suff[n - 1] = 1;
+        for (let i = 1; i < n; i++) {
+            pref[i] = nums[i - 1] * pref[i - 1];
+        }
+        for (let i = n - 2; i >= 0; i--) {
+            suff[i] = nums[i + 1] * suff[i + 1];
+        }
+        for (let i = 0; i < n; i++) {
+            res[i] = pref[i] * suff[i];
+        }
+        return res;
+    }
+}
+
+// Prefix and Suffix -- Optimal
+Time complexity: O(n)
+Space complexity: O(1)
+
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+        const n = nums.length;
+        const res = new Array(n).fill(1);
+
+        for (let i = 1; i < n; i++) {
+            res[i] = res[i - 1] * nums[i - 1];
+        }
+        
+        let postfix = 1;
+        for (let i = n - 1; i >= 0; i--) {
+            res[i] *= postfix;
+            postfix *= nums[i];
+        }
+        return res;
+    }
+}
+
+
